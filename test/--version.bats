3738216fd8c669af1e20802ea9f330926ca79db5
neymarjohn <seriousman1994217winter@gmail.com>
2014-03-13 20:40:47 +0100

    Use `mktemp -d` for RBENV_TEST_DIR

--- test/--version.bats
+++ test/--version.bats
@@ -13,34 +13,34 @@ git_commit() {
 }
 
 @test "default version" {
-  assert [ ! -e "$PYENV_ROOT" ]
-  run pyenv---version
+  assert [ ! -e "$RBENV_ROOT" ]
+  run rbenv---version
   assert_success
-  [[ $output == "pyenv 0."* ]]
+  [[ $output == "rbenv 0."* ]]
 }
 
 @test "reads version from git repo" {
-  mkdir -p "$PYENV_ROOT"
-  cd "$PYENV_ROOT"
+  mkdir -p "$RBENV_ROOT"
+  cd "$RBENV_ROOT"
   git init
   git_commit
   git tag v0.4.1
   git_commit
   git_commit
 
-  cd "$PYENV_TEST_DIR"
-  run pyenv---version
+  cd "$RBENV_TEST_DIR"
+  run rbenv---version
   assert_success
-  [[ $output == "pyenv 0.4.1-2-g"* ]]
+  [[ $output == "rbenv 0.4.1-2-g"* ]]
 }
 
 @test "prints default version if no tags in git repo" {
-  mkdir -p "$PYENV_ROOT"
-  cd "$PYENV_ROOT"
+  mkdir -p "$RBENV_ROOT"
+  cd "$RBENV_ROOT"
   git init
   git_commit
 
-  cd "$PYENV_TEST_DIR"
-  run pyenv---version
-  [[ $output == "pyenv 0."* ]]
+  cd "$RBENV_TEST_DIR"
+  run rbenv---version
+  [[ $output == "rbenv 0."* ]]
 }
--- test/test_helper.bash
+++ test/test_helper.bash
@@ -1,30 +1,30 @@
-unset PYENV_VERSION
-unset PYENV_DIR
+unset RBENV_VERSION
+unset RBENV_DIR
 
-PYENV_TEST_DIR="${BATS_TMPDIR}/pyenv"
+RBENV_TEST_DIR="$(mktemp -d --tmpdir=$BATS_TMPDIR rbenv.bats.XXX)"
 
 # guard against executing this block twice due to bats internals
-if [ "$PYENV_ROOT" != "${PYENV_TEST_DIR}/root" ]; then
-  export PYENV_ROOT="${PYENV_TEST_DIR}/root"
-  export HOME="${PYENV_TEST_DIR}/home"
+if [ "$RBENV_ROOT" != "${RBENV_TEST_DIR}/root" ]; then
+  export RBENV_ROOT="${RBENV_TEST_DIR}/root"
+  export HOME="${RBENV_TEST_DIR}/home"
 
   PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
-  PATH="${PYENV_TEST_DIR}/bin:$PATH"
+  PATH="${RBENV_TEST_DIR}/bin:$PATH"
   PATH="${BATS_TEST_DIRNAME}/../libexec:$PATH"
   PATH="${BATS_TEST_DIRNAME}/libexec:$PATH"
-  PATH="${PYENV_ROOT}/shims:$PATH"
+  PATH="${RBENV_ROOT}/shims:$PATH"
   export PATH
 fi
 
 teardown() {
-  rm -rf "$PYENV_TEST_DIR"
+  rm -rf "$RBENV_TEST_DIR"
 }
 
 flunk() {
   { if [ "$#" -eq 0 ]; then cat -
     else echo "$@"
     fi
-  } | sed "s:${PYENV_TEST_DIR}:TEST_DIR:g" >&2
+  } | sed "s:${RBENV_TEST_DIR}:TEST_DIR:g" >&2
   return 1
 }
 
@@ -95,15 +95,15 @@ assert() {
 }
 
 # Output a modified PATH that ensures that the given executable is not present,
-# but in which system utils necessary for pyenv operation are still available.
+# but in which system utils necessary for rbenv operation are still available.
 path_without() {
   local exe="$1"
   local path="${PATH}:"
   local found alt util
   for found in $(which -a "$exe"); do
     found="${found%/*}"
-    if [ "$found" != "${PYENV_ROOT}/shims" ]; then
-      alt="${PYENV_TEST_DIR}/$(echo "${found#/}" | tr '/' '-')"
+    if [ "$found" != "${RBENV_ROOT}/shims" ]; then
+      alt="${RBENV_TEST_DIR}/$(echo "${found#/}" | tr '/' '-')"
       mkdir -p "$alt"
       for util in bash head cut readlink greadlink; do
         if [ -x "${found}/$util" ]; then
